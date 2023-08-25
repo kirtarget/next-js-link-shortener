6326d386d0e5ffccccf9e1867496063c5118009f
kirtarget <sheldongriffiths1@gmail.com>
2023-08-25 13:32:37 +0300

    Исправил ошибки

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,6 +45,7 @@ export default function Home() {
           <div className="flex flex-col items-center gap-2">
             <p className="text-2xl text-white">
               {hello.data ? hello.data.greeting : "Loading tRPC query..."}
+              
             </p>
             <AuthShowcase />
           </div>
