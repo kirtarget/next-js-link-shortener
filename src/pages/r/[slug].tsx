44b9b7e4ab466b3d6283e12a41c8639f8d3d8ded
kirtarget <sheldongriffiths1@gmail.com>
2023-08-28 00:04:51 +0300

    Сделал охеренный раутинг

--- src/pages/r/[slug].tsx
+++ src/pages/r/[slug].tsx
@@ -1,88 +1,32 @@
-import { useRouter } from "next/router";
 import { prisma } from "../../server/db";
-import { createServerSideHelpers } from "@trpc/react-query/server";
-import { appRouter } from "~/server/api/root";
-import superjson from "superjson";
-import type {
-  GetStaticPaths,
-  GetStaticPropsContext,
-  InferGetStaticPropsType,
-} from "next";
-import { trpc } from "~/utils/trpc";
+import type { NextApiRequest } from "next";
 
-const Slug = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
-  const router = useRouter();
-  const { slug }: { slug: string | undefined } = props;
-  const linkQuery = trpc.links.getOne.useQuery({
-    shortLink: slug!,
+export const getServerSideProps = async (context: NextApiRequest) => {
+  const slug = context.query.slug as string;
+
+  const url = await prisma.shortLink.findUnique({
+    where: {
+      shortLink: slug,
+    },
   });
-  // trpc.links.incrementClick.useMutation().mutate({ shortLink: slug ?? "" });
-  if (linkQuery.status !== "success") {
-    // won't happen since we're using `fallback: "blocking"`
-    return <></>;
+
+  const encodedUrl = encodeURI(url!.link);
+
+  if (url?.link) {
+    return {
+      redirect: {
+        destination: encodedUrl,
+        permanent: false,
+      },
+    };
   }
-  const { data } = linkQuery;
-  void router.replace(data ?? "/404");
+  return {
+    props: {},
+  };
+};
 
-  return <></>;
-  // const [didMount, setDidMount] = useState<boolean>(false);
-  // const mutation = api.links.incrementClick.useMutation();
-  // const path = router.asPath.slice(3);
-  // const link = api.links.getOne.useQuery({
-  //   shortLink: path,
-  // });
-
-  // useEffect(() => {
-  //   if (path === "[slug]") return;
-
-  //   if (typeof link.data !== undefined) {
-  //     void mutation.mutate({ shortLink: path });
-  //
-  //     return;
-  //   }
-  // }, [link]);
-
-  return <></>;
+const Slug = () => {
+  return <p>Такого адреса не существует 🥲</p>;
 };
 
 export default Slug;
-
-export async function getStaticProps(
-  context: GetStaticPropsContext<{ slug: string }>
-) {
-  const helpers = createServerSideHelpers({
-    router: appRouter,
-    ctx: {
-      session: null,
-      prisma,
-    },
-    transformer: superjson, // optional - adds superjson serialization
-  });
-  const slug = context.params?.slug;
-  // prefetch `post.byId`
-  await helpers.links.getAllSlugs.prefetch();
-  return {
-    props: {
-      trpcState: helpers.dehydrate(),
-      slug,
-    },
-    revalidate: 1,
-  };
-}
-export const getStaticPaths: GetStaticPaths = async () => {
-  const links = await prisma.shortLink.findMany({
-    select: {
-      shortLink: true,
-    },
-  });
-
-  return {
-    paths: links.map((link) => ({
-      params: {
-        slug: link.shortLink,
-      },
-    })),
-    // https://nextjs.org/docs/pages/api-reference/functions/get-static-paths#fallback-blocking
-    fallback: "blocking",
-  };
-};
