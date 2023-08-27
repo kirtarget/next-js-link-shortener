3c61c52de3579bdc8ee532e2621acd5b26bd6dc3
kirtarget <sheldongriffiths1@gmail.com>
2023-08-27 14:00:32 +0300

    Корявенько сделал инкрементацию кликов

--- src/pages/r/[slug].tsx
+++ src/pages/r/[slug].tsx
@@ -3,14 +3,23 @@ import { useEffect, useState } from "react";
 import { api } from "~/utils/api";
 
 const Slug = () => {
-  const [didMount, setDidMount] = useState<boolean>(false);
+  // const [didMount, setDidMount] = useState<boolean>(false);
   const router = useRouter();
-  const link = api.links.getOne.useQuery({ shortLink: router.asPath.slice(3) });
-  useEffect(() => {
-    setDidMount(true);
-  }, []);
+  const mutation = api.links.incrementClick.useMutation();
+  const path = router.asPath.slice(3);
+  const link = api.links.getOne.useQuery({
+    shortLink: path,
+  });
 
-  if (didMount && link.data) void router.replace(link?.data ?? "/404");
+  useEffect(() => {
+    if (path === "[slug]") return;
+
+    if (typeof link.data !== undefined) {
+      void mutation.mutate({ shortLink: path });
+      void router.replace(link?.data ?? "");
+      return;
+    }
+  }, [link]);
 
   return;
 };
--- src/server/api/routers/links.ts
+++ src/server/api/routers/links.ts
@@ -18,6 +18,25 @@ export const linksRouter = createTRPCRouter({
           shortLink: input?.shortLink || "404",
         },
       });
+
+      return link?.link;
+    }),
+  incrementClick: publicProcedure
+    .input(
+      z.object({
+        shortLink: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const link = await ctx.prisma.shortLink.update({
+        where: {
+          shortLink: input?.shortLink,
+        },
+        data: {
+          clicks: { increment: 1 },
+        },
+      });
+
       return link?.link;
     }),
 
